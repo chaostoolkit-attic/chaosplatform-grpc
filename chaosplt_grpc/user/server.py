7269424721817e78cd1479c7347aab2b4f536a0e
Sylvain Hellegouarch <sh@defuze.org>
2018-12-19 14:47:33 +0100

    Fix import
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- chaosplt_grpc/user/server.py
+++ chaosplt_grpc/user/server.py
@@ -3,7 +3,7 @@ from typing import List, NoReturn
 import grpc
 from grpc import Channel, Server
 
-from .auth_pb2_grpc import add_UserServiceServicer_to_server, \
+from .user_pb2_grpc import add_UserServiceServicer_to_server, \
     UserServiceServicer
 from .message import CreateRequest, CreateReply, DeleteRequest, DeleteReply, \
     GetByUserRequest, GetByUserReply, User
