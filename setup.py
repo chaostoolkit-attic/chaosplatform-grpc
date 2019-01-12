5fbc9840090eb162388582714b5d4beb49cdd63a
Sylvain Hellegouarch <sh@defuze.org>
2019-01-12 17:07:22 +0100

    Fix setup
    Signed-off-by: Sylvain Hellegouarch <sh@defuze.org>

--- setup.py
+++ setup.py
@@ -28,6 +28,17 @@ def read(*names, **kwargs) -> str:
     ).read()
 
 
+needs_pytest = set(['pytest', 'test']).intersection(sys.argv)
+pytest_runner = ['pytest_runner'] if needs_pytest else []
+test_require = []
+with io.open('requirements-dev.txt') as f:
+    test_require = [l.strip() for l in f if not l.startswith('#')]
+
+install_require = []
+with io.open('requirements.txt') as f:
+    install_require = [l.strip() for l in f if not l.startswith('#')]
+
+
 setup(
     name='chaosplatform-grpc',
     version=get_version_from_package(),
@@ -40,6 +51,9 @@ setup(
     url='https://github.com/chaostoolkit/chaosplatform-grpc',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=install_require,
+    tests_require=test_require,
+    setup_requires=pytest_runner,
     zip_safe=False,
     python_requires='>=3.6.*',
     classifiers=[
